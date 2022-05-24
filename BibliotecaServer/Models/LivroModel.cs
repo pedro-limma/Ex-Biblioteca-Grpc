adf2e8eeb1ab3fb4c83e26a0bf7dbc84a3824fa5
palima3 <palima3@STFC047247-L>
2022-05-24 00:15:35 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaServer/Models/LivroModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BibliotecaServer.Models
+{
+    public class LivroModel
+    {
+        public LivroModel(int id, string nome, string tipo)
+        {
+            Id = id;
+            Nome = nome;
+            Tipo = tipo;
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ BibliotecaServer/Services/BibliotecaService.cs
@@ -0,0 +1,71 @@
+﻿using BibliotecaServer;
+using BibliotecaServer.Models;
+using Grpc.Core;
+
+namespace BibliotecaServer.Services
+{
+    public class BibliotecaService : BibliotecaServiceGrpc.BibliotecaServiceGrpcBase
+    {
+        public static readonly List<LivroModel> Biblioteca = new List<LivroModel>
+        {
+            new LivroModel(0, "As 20 mil léguas submarinas", "Ficção" ),
+            new LivroModel(1, "Viagem ao centro da Terra", "Ficção" ),
+            new LivroModel(2, "It: a coisa", "Ficção" ),
+        };
+
+        public override Task<Livros> SelectAll(Empty request, ServerCallContext context)
+        {
+            Livros responseData = new Livros();
+            var query = from liv in Biblioteca
+                        select new Livro()
+                        {
+                            LivroID = liv.Id,
+                            Name = liv.Nome,
+                            Tipo = liv.Tipo
+                        };
+
+            responseData.Items.AddRange(query.ToArray());
+
+            return Task.FromResult(responseData);
+        }
+
+        public override Task<Empty> Insert(Livro request, ServerCallContext context)
+        {
+            Biblioteca.Add(new LivroModel(request.LivroID, request.Name, request.Tipo));
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Livro> SelectByID(LivroFilter request, ServerCallContext context)
+        {
+            var data = Biblioteca.Find(livro => livro.Id == request.LivroID);
+
+            Livro livro = new Livro() { LivroID = data.Id, Name = data.Nome, Tipo = data.Tipo };
+            
+            return Task.FromResult(livro);
+        }
+
+        public override Task<Empty> Update(Livro request, ServerCallContext context)
+        {
+            var data = Biblioteca.FirstOrDefault(livro => livro.Id == request.LivroID);
+
+            if (data is not null)
+            {
+                data.Nome = request.Name;
+                data.Tipo = request.Tipo;
+            }
+
+            return Task.FromResult(new Empty());
+        }
+
+        public override Task<Empty> Delete(LivroFilter request, ServerCallContext context)
+        {
+            var data = Biblioteca.FirstOrDefault(livro => livro.Id == request.LivroID);
+
+            if(data is not null)
+                Biblioteca.Remove(data);
+
+            return Task.FromResult(new Empty());
+        }
+    }
+}
